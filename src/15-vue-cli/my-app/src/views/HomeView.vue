f286ba3d79961e1c838ffd155a01333aca4acb0b
Jhonny Ribeiro <jaribeiro32@gmail.com>
2022-04-05 12:11:53 -0300

    programatic navigation

--- src/15-vue-cli/my-app/src/views/HomeView.vue
+++ src/15-vue-cli/my-app/src/views/HomeView.vue
@@ -13,6 +13,14 @@ export default {
     return {};
   },
 
+  created() {
+    // this.$router.push("/a-empresa");
+    // this.$router.push({ name: "team"});
+    // this.$router.push({ name: "team", query: { course: "vuejs" } });
+    // this.$router.push({ name: "team", hash: "#vue" });
+    this.$router.push({ name: "team", params: { member: "jhow" } });
+  },
+
   methods: {},
 };
 </script>
