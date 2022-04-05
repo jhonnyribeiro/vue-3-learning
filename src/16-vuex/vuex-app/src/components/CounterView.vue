1bc09df1341c1dc8da7d7de94628b6e77b3362df
Jhonny Ribeiro <jaribeiro32@gmail.com>
2022-04-05 18:18:26 -0300

    mapMutations 1

--- src/16-vuex/vuex-app/src/components/CounterView.vue
+++ src/16-vuex/vuex-app/src/components/CounterView.vue
@@ -1,16 +1,16 @@
 <template>
   <div class="input-group mb-3">
     <button
-      @click.stop.prevent="decrement()"
+      @click.stop.prevent="decrement(10)"
       class="btn btn-outline-secondary"
       type="button"
       id="button-addon1"
     >
       -
     </button>
-    <input :value="$store.state.counter" type="text" class="form-control" />
+    <input :value="counter" type="text" class="form-control" />
     <button
-      @click.stop.prevent="increment()"
+      @click.stop.prevent="increment(10)"
       class="btn btn-outline-secondary"
       type="button"
       id="button-addon1"
@@ -21,6 +21,7 @@
 </template>
 
 <script>
+import { mapState, mapMutations } from "vuex";
 export default {
   name: "CounterView",
   data() {
@@ -31,13 +32,16 @@ export default {
   //     this.$store.commit("increment");
   //   },
 
+  computed: { ...mapState({ counter: (state) => state.counter }) },
+
   methods: {
-    increment() {
-      this.$store.commit("increment", 10);
-    },
-    decrement() {
-      this.$store.commit("decrement", 10);
-    },
+    ...mapMutations(["increment", "decrement"]),
+    // increment() {
+    //   this.$store.commit("increment", 10);
+    // },
+    // decrement() {
+    //   this.$store.commit("decrement", 10);
+    // },
   },
 };
 </script>
