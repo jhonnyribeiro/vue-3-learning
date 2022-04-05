49cf749e9808f4c9443f1b8256c7829b06b1e9f6
Jhonny Ribeiro <jaribeiro32@gmail.com>
2022-04-05 15:43:48 -0300

    install vuex

--- src/16-vuex/vuex-app/src/main.js
+++ src/16-vuex/vuex-app/src/main.js
@@ -1,5 +1,22 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router";
+import { createStore } from "vuex";
 
-createApp(App).use(router).mount('#app')
+const store = createStore({
+  state() {
+    return {
+      first_name: "Jhon",
+      last_name: "snow",
+      email: "js@local.com",
+    };
+  },
+
+  mutation: {
+    updateName() {},
+  },
+});
+
+createApp(App).use(store).use(router).mount("#app");
+
+console.log(store.state.email);
